f15e86532fabdf933f7a1fbf731724b01f5d66fc
rianesmili <rianesmili@gmail.com>
2023-01-04 16:20:56 +0100

    Amelioration of activity_main.xml and MainActivity.kt

--- app/src/main/java/com/example/estimmo/MainActivity.kt
+++ app/src/main/java/com/example/estimmo/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.estimmo
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.estimmo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startButton.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Bienvenue a EST'IMMO", Toast.LENGTH_LONG).show()
+            /*
+            val intent = Intent(this@MainActivity, MainActivity::class.java)
+            startActivity(intent)
+             */
+
+
+        }
     }
 }

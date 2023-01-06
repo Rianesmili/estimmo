7e37ef3bf661cb663dcc4b3d4a02e21b89be1175
smili Riane <riane.smili@univ-rouen.fr>
2023-01-06 19:24:12 +0000

    Revert "now the functions in application are correctly working"
    This reverts commit 81473628d12c91b5d952c084e5eb2397a78faa50

--- app/src/main/java/com/example/estimmo/backend/ImmoBackendImpl.kt
+++ app/src/main/java/com/example/estimmo/backend/ImmoBackendImpl.kt
@@ -6,21 +6,17 @@ import javax.inject.Inject
 class ImmoBackendImpl @Inject constructor():ImmoBackendInterface{
 
     override fun calculPriceHome(x1: Double, x2: Double, x3: Double, x4: Double): Double {
-
-        val coef = doubleArrayOf(0.00000000e+00 , 1.50168101e+03,  1.82025034e+04,  1.06263368e+02,
-            -2.21840948e+02, -1.23017240e+02,  4.66231874e-01,  7.97807793e+00,
-            -2.32467828e+01, -9.86832447e+01,  2.80505641e-01)
-        val intercept = -14607.256792391738
+        val coef = doubleArrayOf(0.0, 1337.33905, 18442.7549, 88.8857886, -73.1976145, -130.13074, 0.397920667, 17.5122482, -14.7366072, -216.105674, 0.604713373)
+        val intercept = -3090.8531835110043
         val price = intercept + coef[1]*x1 + coef[2]*x2 + coef[3]*x3 + coef[4]*x4 + coef[5]*x1*x2 + coef[6]*x1*x3 + coef[7]*x1*x4 + coef[8]*x2*x3  + coef[9]*x2*x4 + coef[10]*x3*x4
         return price
     }
 
     override fun calculPriceApt(x1: Double, x2: Double, x3: Double, x4: Double): Double {
-        val coef1 = doubleArrayOf(0.00000000e+00,  8.26970326e+03, 3.21642464e+05,  2.54519530e+02,
-            1.73528223e+04, -3.09132103e+03 , 1.97105670e+00, -3.83197257e+02, -1.25830078e+02, -3.34302865e+02 , 5.69821496e+00)
+        val coef1 = doubleArrayOf(0.0, 8269.70326, 32164.2464, 25.4519530, 17352.8223, -3091.32103, 1.97105670, -383.197257, -125.830078, -334.302865, 5.69821496)
         val intercept1 = -234383.09085176938
-        val    equation = intercept1 + coef1[1]*x1 + coef1[2]*x2 + coef1[3]*x3 + coef1[4]*x4 + coef1[5]*x1*x2 + coef1[6]*x1*x3+ coef1[7]*x1*x4 + coef1[8]*x2*x3 + coef1[9]*x2*x4+ coef1[10]*x3*x4
-        return equation
+        val price1 = intercept1 + coef1[1] * x1 + coef1[2] * x2 + coef1[3] * x3 + coef1[4] * x4 + coef1[5] * x1 * x2 + coef1[6] * x1 * x3 + coef1[7] * x1 * x4 + coef1[8] * x2 * x3 + coef1[9] * x2 * x4 + coef1[10] * x3 * x4
+        return price1
     }
 
 

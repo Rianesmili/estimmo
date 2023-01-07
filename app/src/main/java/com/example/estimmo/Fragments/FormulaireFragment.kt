6eb737df9a6f709b394041354f74af2ad281f95a
rianesmili <rianesmili@gmail.com>
2023-01-07 02:22:55 +0100

    Final version on the App

--- app/src/main/java/com/example/estimmo/Fragments/FormulaireFragment.kt
+++ app/src/main/java/com/example/estimmo/Fragments/FormulaireFragment.kt
@@ -13,6 +13,7 @@ import com.example.estimmo.databinding.FragmentFormulaireBinding
 import com.example.estimmo.viewmodel.ViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import com.example.estimmo.backend.ImmoBackendInterface
+import java.lang.Math.round
 
 
 @AndroidEntryPoint
@@ -33,7 +34,7 @@ class FormulaireFragment : Fragment(R.layout.fragment_formulaire) {
         // var shotType = binding.spinner
 
         val shotType = view?.findViewById<Spinner>(R.id.spinner)
-        //creation du spinner pour choisir le type de bien Appartement ou Maison
+        // Creation du spinner pour choisir le type de bien Appartement ou Maison
         this.context?.let {
             ArrayAdapter.createFromResource(
                 it,
@@ -73,14 +74,6 @@ class FormulaireFragment : Fragment(R.layout.fragment_formulaire) {
             viewModel.parcelle = binding.spinnerParcelle.selectedItemPosition.toString()
             viewModel.type = binding.spinner.selectedItem.toString()
 
-
-
-
-
-
-
-
-
             //For the Condition
 
             val type_logement : String = binding.spinner.getSelectedItem().toString()
@@ -91,35 +84,35 @@ class FormulaireFragment : Fragment(R.layout.fragment_formulaire) {
             val X4 = binding.spinnerParcelle.selectedItemPosition.toDouble()
 
             if (type_logement.equals("Appartement")) {
-                    //Si le choix est un appartement alors
+                //Si le choix est un appartement alors
                 fun calculPriceApt(x1: Double, x2: Double, x3: Double, x4: Double): Double {
 
                     val coef1 = doubleArrayOf(0.00000000e+00,  8.26970326e+03, 3.21642464e+05,  2.54519530e+02,
                         1.73528223e+04, -3.09132103e+03 , 1.97105670e+00, -3.83197257e+02, -1.25830078e+02, -3.34302865e+02 , 5.69821496e+00)
                     val intercept1 = -234383.09085176938
                     val    equation = intercept1 + coef1[1]*x1 + coef1[2]*x2 + coef1[3]*x3 + coef1[4]*x4 + coef1[5]*x1*x2 + coef1[6]*x1*x3+ coef1[7]*x1*x4 + coef1[8]*x2*x3 + coef1[9]*x2*x4+ coef1[10]*x3*x4
-                        return equation
+                    return equation
                 }
                 val result = calculPriceApt(X1, X2, X3, X4)
-                viewModel.estimation = result.toString()
+                val y = round(result)
+                viewModel.estimation = y.toString()
 
-                }else{
-                    //Si le choix est une maison on lance cette fonction
-                    fun calculPriceHome(x1: Double, x2: Double, x3: Double, x4: Double): Double {
+            }else{
+                //Si le choix est une maison on lance cette fonction
+                fun calculPriceHome(x1: Double, x2: Double, x3: Double, x4: Double): Double {
 
-                        val coef = doubleArrayOf(0.00000000e+00 , 1.50168101e+03,  1.82025034e+04,  1.06263368e+02,
-                            -2.21840948e+02, -1.23017240e+02,  4.66231874e-01,  7.97807793e+00,
-                            -2.32467828e+01, -9.86832447e+01,  2.80505641e-01)
-                        val intercept = -14607.256792391738
-                        val price = intercept + coef[1]*x1 + coef[2]*x2 + coef[3]*x3 + coef[4]*x4 + coef[5]*x1*x2 + coef[6]*x1*x3 + coef[7]*x1*x4 + coef[8]*x2*x3  + coef[9]*x2*x4 + coef[10]*x3*x4
-                        return price
-                    }
+                    val coef = doubleArrayOf(0.00000000e+00 , 1.50168101e+03,  1.82025034e+04,  1.06263368e+02,
+                        -2.21840948e+02, -1.23017240e+02,  4.66231874e-01,  7.97807793e+00,
+                        -2.32467828e+01, -9.86832447e+01,  2.80505641e-01)
+                    val intercept = -14607.256792391738
+                    val price = intercept + coef[1]*x1 + coef[2]*x2 + coef[3]*x3 + coef[4]*x4 + coef[5]*x1*x2 + coef[6]*x1*x3 + coef[7]*x1*x4 + coef[8]*x2*x3  + coef[9]*x2*x4 + coef[10]*x3*x4
+                    return price
+                }
+
                 val result = calculPriceHome(X1, X2, X3, X4)
-                viewModel.estimation = result.toString()
+                val x = round(result)
+                viewModel.estimation = x.toString()
             }
-
-
-
 
             findNavController().navigate(R.id.action_formulaireFragment_to_resultFragment)
         }
